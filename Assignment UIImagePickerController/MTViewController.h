9e6ff6423143f55b086b454f4d3814bbd6cf2195
Michael Tirenin <michaeltirenin@Michaels-MacBook-Pro-2.local>
2014-05-18 18:06:15 -0700

    Initial Commit

---
+++ Assignment UIImagePickerController/MTViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTViewController.h
+//  Assignment UIImagePickerController
+//
+//  Created by Michael Tirenin on 5/18/14.
+//  Copyright (c) 2014 Michael Tirenin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTViewController : UIViewController
+
+@end
